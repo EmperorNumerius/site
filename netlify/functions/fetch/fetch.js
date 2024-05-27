b3c64c147b7ebdf76c71254c0665e6f67f738e3a
Emperor Numerius <62899740+EmperorNumerius@users.noreply.github.com>
2024-05-26 23:40:50 -0600

    I DONT KNOW IF THIS WILL WORK

--- netlify/functions/fetch/fetch.js
+++ netlify/functions/fetch/fetch.js
@@ -1,48 +1,44 @@
-// fetch.js
 const axios = require("axios");
-const qs = require("qs"); // Add this line
 
 exports.handler = async function (event, context) {
   try {
-    // Parse the incoming form data from the request body
-    const { name, email, gdpr, hp, list, subform, boolean } = JSON.parse(
-      event.body,
-    );
+    const { name, email, gdpr, hp, list, subform } = JSON.parse(event.body);
 
-    // Stringify the data into a query string
-    const data = qs.stringify({
-      name,
-      email,
-      gdpr,
-      hp,
-      list,
-      subform,
-      boolean,
-    });
+    const data = {
+      personalizations: [
+        {
+          to: [{ email: email }],
+          subject: "New RSVP Submission",
+        },
+      ],
+      from: { email: "team@purplebubble.org" },
+      content: [
+        {
+          type: "text/plain",
+          value: `Name: ${name}\nEmail: ${email}\nGDPR: ${gdpr}\nHP: ${hp}\nList: ${list}\nSubform: ${subform}`,
+        },
+      ],
+    };
 
-    // Make the cURL request using axios
     const response = await axios.post(
-      "https://postal.hackclub.com/subscribe",
+      "https://api.mailchannels.net/tx/v1/send",
       data,
       {
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
+          "Content-Type": "application/json",
         },
-      },
+      }
     );
 
-    // log the response for debugging purposes
     console.log(response.data);
 
-    // Return the response from the external API
     return {
       statusCode: response.status,
       body: JSON.stringify(response.data),
     };
   } catch (error) {
-    // Return an error response if something goes wrong
-    // log the error for debugging purposes
     console.log(error);
+
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Internal Server Error" }),
